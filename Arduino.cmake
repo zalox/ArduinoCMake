0844585be1a292cc0ffcd766bd77cacc38d78375
Jørgen Lien Sellæg <jorgen@guut.org>
2016-04-21 12:14:48 +0200

    Add fork source to contributors of this project

--- Arduino.cmake
+++ Arduino.cmake
@@ -1,10 +1,11 @@
-# This file is based on the work of:
+# This project is based on the work of:
 # https://github.com/tmpsantos/Arduino-CMake-Template
 # http://mjo.tc/atelier/2009/02/arduino-cli.html
 # http://johanneshoff.com/arduino-command-line.html
 # http://www.arduino.cc/playground/Code/CmakeBuild
 # http://www.tmpsantos.com.br/en/2010/12/arduino-uno-ubuntu-cmake/
 # http://forum.arduino.cc/index.php?topic=244741.0
+# https://github.com/ChisholmKyle/ArduinoCMake
 
 # set compilers
 find_program(AVR_GCC avr-gcc)
